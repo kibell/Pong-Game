fbc0490e6de615e5ad283fea5b8d600567e2773c
Kitrick Bell <kitrickd.bell@gmail.com>
2021-12-29 18:18:38 -0600

    created basic setup using the turle module

---
+++ app.py
@@ -0,0 +1,20 @@
+import turtle
+wn = turtle.Screen()
+
+wn.title("Pong by Kitrick")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+#paddle A
+
+
+#paddle B
+
+#ball
+
+
+
+#main game loop
+while True:
+    wn.update()
