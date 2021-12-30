4b9ef7ceadaa5a187be3e81a8dafa3be290a1bde
Kitrick Bell <kitrickd.bell@gmail.com>
2021-12-29 18:31:41 -0600

    created paddle a and paddle b, set the position and sizes for the objects

--- app.py
+++ app.py
@@ -7,9 +7,24 @@ wn.setup(width=800, height=600)
 wn.tracer(0)
 
 #paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
 
 
 #paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
 
 #ball
 
